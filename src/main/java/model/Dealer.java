16ec29044be04f2db7a4460220529eb4aacaf61d
tri2sing <tri2sing@yahoo.com>
2015-03-08 16:44:02 -0700

    Added the Dealer class

---
+++ src/main/java/model/Dealer.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Dealer {
+	private Deck deck;
+	
+	public Dealer () {
+		deck = new Deck();
+	}
+	
+	public Card dealCard() {
+		if(deck.size() > 0) {
+			return deck.getCardFromFront();
+		}
+		else {
+			return null;
+		}
+	}
+	
+	public void shuffleDeck() {
+		int numCards = deck.size();
+		for(int i = 0; i < numCards; i++) {
+			int j = (int) (Math.random() * numCards);
+			if(j != i)
+				deck.swapCards(i, j);
+		}
+	}
+}
