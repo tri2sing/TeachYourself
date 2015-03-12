2c5c2370137901468f19d32f4aed32b5d7c876df
tri2sing <tri2sing@yahoo.com>
2015-03-12 16:45:13 -0700

    Added the next example

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,31 @@
+package bank;
+
+public class Account {
+	private double balance;
+
+	public Account(double balance) {
+		this.balance = balance;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		if(balance >= amount) {
+			balance -= amount;
+		}
+		else {
+			throw new IllegalArgumentException("The amount to withdraw is greater than the balance");
+		}
+	}
+
+}
