48690fdcd779b0f6e6b6b51119d558e1319f7326
tri2sing <tri2sing@yahoo.com>
2015-05-11 14:53:50 -0700

    continued working

--- src/main/java/blackjack/Deck.java
+++ src/main/java/blackjack/Deck.java
@@ -1,15 +1,61 @@
 package blackjack;
 
+import java.util.Iterator;
 
-public class Deck {
-	private Card [] deck;
-	
+public class Deck implements Iterable<Card> {
+	private Card[] deck;
+
 	public Deck() {
+		// I know there are 52 cards in a deck, but this is just to 
+		// increase my knowledge of the API so using length.
+		deck = new Card[Suit.values().length * Rank.values().length];
 		int i = 0;
-		for (Suit s: Suit.values()) {
-			for (Rank r: Rank.values()) {
-				deck[i] = new Card(s, r); 
+		for (Suit s : Suit.values()) {
+			for (Rank r : Rank.values()) {
+				deck[i++] = new Card(s, r);
 			}
 		}
 	}
+
+	@Override
+	public Iterator<Card> iterator() {
+		return new DeckIterator(deck);
+	}
+
+	private class DeckIterator implements Iterator<Card> {
+		private int index;
+		Card[] cards;
+
+		public DeckIterator(Card[] cards) {
+			this.cards = cards;
+			int index = 0;
+		}
+
+		@Override
+		public boolean hasNext() {
+			if (index < cards.length)
+				return true;
+			else
+				return false;
+		}
+
+		@Override
+		public Card next() {
+			return cards[index++];
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException("Deck Iterator does not allow this operation");
+		}
+
+	}
+	
+	public static void main(String [] args) {
+		Deck deck = new Deck();
+		for(Card c: deck) {
+			System.out.println(c.toString());
+		}
+	}
+
 }
---
+++ src/main/java/blackjack/Pile.java
@@ -0,0 +1,61 @@
+package blackjack;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Pile {
+	private int index;
+	private ArrayList<Card> pile;
+	private Random random;
+
+	public Pile() {
+		index = 0;
+		random = new Random();
+		pile = new ArrayList<Card>();
+	}
+
+	public void addDeck(Deck deck) {
+		for (Card c : deck)
+			pile.add(c);
+	}
+	
+	public Card deal(Face face) {
+		// Right now I depend on the default IndexOutOfBoundsException exception
+		Card card = pile.get(index++);  
+		card.setFace(face);
+		return card;
+	}
+	
+	public void shuffle(int numTimes) {
+		reset();
+		for(int i = 0; i < numTimes; i++)
+			shuffle();
+	}
+	
+	private void reset() {
+		for(Card card: pile) 
+			card.setFace(Face.DOWN);
+	}
+	
+	private void shuffle() {
+		int numCards = pile.size();
+		for (int current = 0; current < numCards; current++) {
+			int target = random.nextInt(numCards);
+			Card currentCard = pile.get(current);
+			Card targetCard = pile.get(target);
+			pile.set(current, targetCard);
+			pile.set(target, currentCard);
+		}
+	}
+	
+	public static void main(String [] args) {
+		Deck deck = new Deck();
+		Pile pile = new Pile();
+		pile.addDeck(deck);
+		pile.shuffle(1);
+		while(true) {
+			System.out.println(pile.deal(Face.DOWN).toString());
+		}
+	}
+
+}
